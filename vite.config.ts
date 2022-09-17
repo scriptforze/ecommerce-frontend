d4a817857dbcf100874c8fa006c6f1cc2191e108
Juan David Garcia <juancho637@gmail.com>
2022-09-16 23:38:43 -0500

    removing some settings from vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ecommerce-frontend/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
