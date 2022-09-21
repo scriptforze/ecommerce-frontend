fc071d18c354cdeba754917c91be1d1d262d9b6a
Juan David Marcillo Alba <juan.marcillo@whiteprompt.com>
2022-09-21 00:32:05 -0500

    Remove unused imports

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,7 +2,6 @@ import { Route } from "react-router-dom";
 import { AuthRoutes } from "@/modules/auth/routes/AuthRoutes";
 import { RoutesWithNotFound } from "@/modules/common/utils/RoutesWithNotFound";
 import { ProductsRoutes } from "@/modules/products/routes/ProductsRoutes";
-import AdminLayout from "@/modules/common/layout/AdminLayout";
 
 export const AppRouter = () => {
   return (
